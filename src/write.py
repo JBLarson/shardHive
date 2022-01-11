673e1b2c318c23bf7e4a21da9e0ddedf57a6a419
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-01-10 19:28:44 -0500

    Update write.py

--- src/write.py
+++ src/write.py
@@ -1,30 +1,101 @@
-#!/Users/jb/Desktop/web3/altCoin/jb3nv/bin/python3
+#!/Users/jb/Desktop/shardHive/hiveEnv/bin/python3
 
 from shardFuncs import *
+import sys
+import cryptography
+from cryptography.fernet import Fernet
+import os
 
-theString = 'Data that will be sharded and encrypted'
+#fileInput = sys.argv[1]
+#fileInput = 'theString.txt'
+fileInput = 'testJson.json'
 
-shards = shardString(theString, 4)
+
+fileType = fileInput.split('.')[1].lower()
+
+
+if fileType == 'txt':
+	fileContent = ''
+	with open(fileInput, 'r') as f:
+		for line in f:
+			fileContent = fileContent + line
+
+elif fileType == 'json':
+	fileContent = readJsonFunc(str(fileInput))
+
+
+shards = shardString(str(fileContent), 8)
 
 
 
-def encryptShards(shardList):
-	
+def createConfig(shardList, userId):
+
+	shardConfig, shardMap = {}, {}
+	# concat userId and dtRn to create key
+	idAndDt = 'id__' + str(userId) + '_dt__' + str(dtRn)
+	currentKeyAddr = keyGen(idAndDt)
+	keyFile = open(currentKeyAddr, 'rb')
+	currentKey = keyFile.read()
+	keyFile.close()
+
+	for shard in shardList:
+		shardIndex = shardList.index(shard)
+
+		jsonAddr = 'data/' + str(shardIndex) + '.json'
+		createCurrentJson = createJsonFunc(jsonAddr, shard)
+
+		with open(jsonAddr, 'rb') as f:
+			currentJson = f.read()
+
+		fernet = Fernet(currentKey)
+		encryptedJson = fernet.encrypt(currentJson)
+
+		encryptedJsonAddr = str(jsonAddr) + '.encrypted'
+
+		with open(encryptedJsonAddr, 'wb') as f:
+			f.write(encryptedJson)
+
+		# remove unencrypted json
+		os.remove(jsonAddr)
+
+
+		shardMap.update({shardIndex: encryptedJsonAddr})
+
+
+
+
+	shardConfig.update({'key': currentKeyAddr, 'dt': dtRn, 'userId': userId, 'map': shardMap})
+
+	configAddr = '_id__' + str(userId) + '_dt__' + str(dtRn) + '.config'
+	saveConfig = createJsonFunc(configAddr, shardConfig)
+
+	return shardConfig
+
+configTest = createConfig(shards, '12')
+#configAddr = '_id__' + str(configTest['userId']) + '_dt__' + str(configTest['dt']) + '.json'
+
+
+print('\n')
+print(configTest)
+
+"""
+
+def encryptShards(shardConfig):
+
+	currentKey = keyGen(shardIndex)
+	currentKeyAddr = str(shardIndex) + '.key'
+	keyFile = open(currentKeyAddr, 'rb')
+	currentKey = keyFile.read()
+	keyFile.close()
+
 	for shard in shardList:
 		shardIndex = str(shardList.index(shard))
-		currentKey = keyGen(shardIndex)
-		print(currentKey)
-		jsonAddr = str(shardIndex) + '.json'
+
+		jsonAddr = 'data/' + str(shardIndex) + '.json'
 		createCurrentJson = createJsonFunc(jsonAddr, shard)
 		print(createCurrentJson)
 
-	for shard in shardList:
-		shardIndex = str(shardList.index(shard))
 
-		currentKeyAddr = str(shardIndex) + '.key'
-		keyFile = open(currentKeyAddr, 'rb')
-		currentKey = keyFile.read()
-		keyFile.close()
 
 
 		currentJsonAddr = str(shardIndex) + '.json'
@@ -41,3 +112,5 @@ def encryptShards(shardList):
 
 
 encryptShardTest = encryptShards(shards)
+
+"""
