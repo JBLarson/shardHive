b96f4df364fbb34e3c2b583683624d2e30a4ccba
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-02-06 19:06:22 -0500

    Update ftpLs.py

--- src/ftpLs.py
+++ src/ftpLs.py
@@ -1,77 +1,40 @@
+#!/usr/bin/python3
 
 
-import ftplib
+import paramiko
+import pysftp
 from ftpCredz import *
-from shardFuncs import *
-import os
 
 
-#print(dtRn)
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None  
 
-inputConfig = '_id__12_dt__01_11_22_18_33_19.config'
-testConfig = readJsonFunc(inputConfig)
-
-# create user and dt variables from config file
-configSplit = inputConfig.split('__')
-configUser = configSplit[1].replace('_dt', '')
-configDT = configSplit[2].replace('.config', '')
-
-
-theFile = 'theString.txt'
-
-
-
-def readFTPcontent(user, ftpServerName):
-
-	data, fileNames = [], []
-
-	ftpServer = ftplib.FTP(ftpServerName['host'])
-	ftpServer.login(ftpServerName['user'], ftpServerName['pass'])
-	#userDir = '/shardHive/' + str(user) + '/' + str(configDT)
-	userDir = '/shardHive/' + str(user) + '/'
-
-	ftpServer.cwd(userDir)
-
-
-	#ftpPWD = ftpServer.pwd()
-	#rint(ftpPWD)
+def readDirs(ftpServerName, shUser):
 
 	try:
-		#dirNames = ftpServer.mlsd('shardHive0/12')
-		dirNames = ftpServer.nlst()
-		print('\nshardHive directories: ' + str(dirNames))
+		conn = pysftp.Connection(host=ftpServerName['host'],port=port, username=ftpServerName['user'], password=ftpServerName['pass'], cnopts=cnopts)
+		print("connection established successfully")
+	except:	print('\nfailed to establish connection to targeted server\n')
+
+	conn.cwd('FTP/shardHive/' + str(shUser))
+
+	userDirs = conn.listdir()
 
 
-
-		if len(dirNames) > 0:
-			for currentDir in dirNames:
-				ftpServer.cwd(str(currentDir) + '/')
-				filematch = '*.*'
-				fileNames = list(ftpServer.nlst(filematch))
-				print('\nContents of: ' + str(currentDir))
-				print(fileNames)
-
-
-				ftpServer.cwd('/shardHive/12')
-		
-
-
-		print(ftpServer.nlst())
-
-		try:	ftpServer.quit()
-		except: pass
-
-
-
-	except Exception as e:
-		print(e)
-
-		try:	ftpServer.quit()
-		except: pass
+	for uDir in userDirs:
+		print('\nContent of: ' + str(uDir))
+		conn.cwd(uDir)
+		print(conn.listdir())
+		conn.cwd('..')
+	conn.close()
 
 
 
 
-writeTest = readFTPcontent('12', pi0)
+#readTest = readDirs(pi0, 12)
+readTest = readDirs(pi1, 12)
+
+
+
 
 print()
