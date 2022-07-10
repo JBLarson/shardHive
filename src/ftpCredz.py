247504ab6b793e3ea88660f451f533c2d5d3d97d
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-10 01:17:29 -0400

    Delete ftpCredz.py

--- src/ftpCredz.py
+++
@@ -1,4 +0,0 @@
-port = 22
-
-pi0 = {'host': 'shardHive0', 'user': 'pi', 'pass': 'tH3PiP@55', 'address': '0xc838C62098f8C4597908BE707130F63eCeC2ffB7'}
-pi1 = {'host': 'shardHive1', 'user': 'pi', 'pass': 'tH3PiP@55', 'address': '0x983C36a518c56b0FCE99AA8a4aed40913DC469CE'}
