33fd9e50f035b8d9ebc360ce06b033500beeb83f
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-17 11:31:08 -0400

    Update index.php

--- src/io/index.php
+++ src/io/index.php
@@ -2,60 +2,89 @@
 $cookie1_name = "username";
 ?>
 <html lang="en">
+
+
   <head>
+
+
     <link rel="stylesheet" href="ioStyles.css">
+
+
   </head>
   <body>
 
-    <div class="headerContainer">
+      <div class="headerContainer">
+        <h1>shardHive Technology</h1>
+        <h2>Custom PHP is responsible for all pages on io.shardhive.com</h2>. 
+        
+        
+        <img src="https://shardhive.com/wp-content/uploads/2022/02/logoTransparent.svg" alt="shardHive Logo">
+        <h3>Welcome to the Machine</h3>
 
-      <div class="grid-item0">
-        <h1>Welcome to<br>shardHive</h1>
+
       </div>
-
-        <div class="grid-item1">
-          <img src="https://shardhive.com/wp-content/uploads/2022/02/logoTransparent.svg" alt="shardHive Logo">
-        </div>
-    </div>
-    <!-- end of header container transactionContainer starts-->
+      <!-- end of header container transactionContainer starts-->
 
 
 
-    <div class="ioHeaderMenu">
-        
-        <button class="button ioHeaderMenuObject" onclick="window.location.href='https://shardhive.com'">shardHive.com</button>
-
-        <button class="button ioHeaderMenuObject" onclick="window.location.href='https://io.shardhive.com/'">IO Landing</button>
-        
-        <button class="button ioHeaderMenuObject" onclick="window.location.href='https://io.shardhive.com/login.php'">IO Login</button>
-
-        <button class="button ioHeaderMenuObject" onclick="window.location.href='https://io.shardhive.com/register.php'"><a>IO Registration</a></button>
-
-    </div>
 
 
-    <h2>Practice</h2>
+
+      <div class="ioHeaderMenu">
+          
+          <button class="button ioHeaderMenuObject" onclick="window.location.href='https://shardhive.com'">shardHive.com</button>
+
+          <button class="button ioHeaderMenuObject" onclick="window.location.href='https://io.shardhive.com/'">IO Landing</button>
+          
+          <button class="button ioHeaderMenuObject" onclick="window.location.href='https://io.shardhive.com/login.php'">IO Login</button>
+
+          <!--button class="button ioHeaderMenuObject" onclick="window.location.href='https://shardhive.com'"><a>shardHive</a></button>
+
+          <button class="button ioHeaderMenuObject" onclick="window.location.href='https://io.shardhive.com/practice.php'"><a>IO PHP Practice</a></button-->
 
 
-    <div>
-      <form action="/hf.php">
-        <label for="username">Username</label>
-        <?php
-          echo '<input type="text" id="username" name="username" value="' . $_COOKIE[$cookie1_name] . '">';
-        ?>
-        <label for="amount">Test Amount</label>
-        <input type="text" id="amount" name="amount" placeholder="Test Amount: $">
 
-        <label for="currency">Currency</label>
-        <select id="currency" name="currency">
+     </div>
 
-          <option value="ETH">ETH</option>
-          <option value="BTC">BTC</option>
+
+    <h2>Submit File</h2>
+    
+
+    <div class="fileSubmission">
+      <form action="/hf.php" method="post" enctype="multipart/form-data">
+          <label for="userId">userId</label>
+          <input type="text" id="userId" name="userId" placeholder="00001">
+          <label for="fileName">File Name</label>
+          <input type="text" id="fileName" name="fileName" placeholder="test.json">
+
+          <label for="fileUpload">File Upload</label>
+          <input type="file" id="fileUpload" name="fileUpload">
+
         </select>
 
-        <input type="submit" value="Submit">
+        <input type="submit" value="uploadFile" name="submit">
       </form>
     </div>
 
   </body>
+  
+  
+
+  <script>
+    function setCookie(cookieName,value,expirationDays){
+      let date = new Date();
+      date.setTime(date.getTime() + (expirationDays*86400000));
+      let expires = "expires=" + date.toUTCString();
+      document.cookie = cookieName + "=" + value + ";" + expires + ";path=/" + ";domain=.io.shardHive.com";
+    }
+
+    let date = new Date();
+    let theDt = date.setTime(date.getTime() + (1200*86400000));
+    let theDate = date.toUTCString();
+
+    let setTest3 = setCookie('jb3', theDate, theDt);
+  </script>
+
+
+  
 </html>
