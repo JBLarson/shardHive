b3792eceb184e44e401357370bdafc981b625bf4
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-17 11:31:28 -0400

    Update hf.php

--- src/io/hf.php
+++ src/io/hf.php
@@ -1,105 +1,134 @@
-<html lang="en">
-
-    <head>
-      <link rel="stylesheet" href="ioStyles.css">
-    </head>
-
+<?php
+  echo '<head>';
+    echo '<link rel="stylesheet" href="ioStyles.css">';
+  echo '</head>';
 
 
-    <body>
-
-      <div class="headerContainer">
-
-        <div class="grid-item0">
-          <h1>shardHive<br>Testing</h1>
-        </div>
-
-        <div class="grid-item1">
-            <img src="https://shardhive.com/wp-content/uploads/2022/02/logoTransparent.svg" alt="shardHive Logo">
-        </div>
-      
-      </div>
-
-      <div style="font-size: 1.5em;" >
-    
-        <a href="https://shardhive.com">shardHive.com</a>
-        <a href="https://io.shardhive.com/">IO landing page</a>
-        <a href="https://io.shardhive.com/login.php">login page</a>
-        <a href="https://io.shardhive.com/register.php">registration page</a>
-    
-      </div>
+$targetDir = "hiveIO/";
+$targetFile = $targetDir . basename($_FILES["fileUpload"]["name"]);
+$uploadOk = 1;
+$theFileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
 
 
-    </body>
+// Check if file already exists
+if (file_exists($targetFile)) {
+  echo "File already exists.";
+  $uploadOk = 0;
+}
 
-	<?php
-	// handleForm.php
-  echo '<div class="transactionContainer">';
-  $priceETH = 2112;
-  $orderSize = $_GET['amount'];
-  $orderCost = $orderSize * $priceETH;
-	echo '<h2>User: ' . $_GET['username'] . '</h2>';
-  echo '<h3>Ordering: ' . $_GET['amount'] . '  ' . $_GET['currency'] . '</h3>';
-  echo '<h3>Price of ETH: $' . $priceETH . '</h3>';
-  echo '<h3>Cost: $' . $orderCost . '</h3>';
-  echo '</div>';
+// Check file size
+if ($_FILES["fileUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
 
-  $cookie1_name = "username";
-  $cookie1_value = $_GET['username'];
-  setcookie($cookie1_name, $cookie1_value, time() + (86400 * 3000), "/");
-  $cookie2_name = "loginDt";
-  $cookie2_value = date("m_d_h_i_sa");
-  setcookie($cookie2_name, $cookie2_value, time() + (86400 * 3000), "/");
+// Allow certain file formats
+if($theFileType != "csv" && $theFileType != "txt" && $theFileType != "xml" && $theFileType != "json" ) {
+  echo "Sorry, only JSON, CSV, TXT, and XML files are allowed.";
+  $uploadOk = 0;
+}
 
-
-  $servername = "ecngx279";
-  $username = "shardh5_jTest";
-  $password = "tH3SqlTestP@55";
-  $dbname = "shardh5_ioTest";
-
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  //$conn = new mysqli($servername, $username, $password);
-
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  echo "Connected successfully<br>";
-
-
-  /*
-  $sql = "INSERT INTO `jTest` (`name`, `userId`, `ethAddr`, `currencyPref`) VALUES ('" . $_GET['name'] . "', '" . $_GET['amount'] . "', '', '" . $_GET['currency'] . "')";
-
-
-  if ($conn->query($sql) === TRUE) {
-      echo 'created new record successfully';
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "<br>Your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $targetFile)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileUpload"]["name"])). " has been uploaded.";
   } else {
-      echo 'error ' . $sql . '<br>' . $conn->error;
+    echo "Sorry, there was an error uploading your file.";
   }
+}
+
+
+
+echo '<br><br>';
+echo 'Attempted to upload' . $targetFile;
+echo '<h2>User: ' . $_FILES['fileName'] . '.</h2>';
+
+//echo '<br><br><br>';
+//$fileContents = readfile($targetFile);
+//echo '<br><br>';
+
+
+$fcFile = fopen($targetFile, "r") or die("unable to open file");
+$fc = fread($fcFile,filesize($targetFile));
+fclose($fcFile);
+
+
+//echo $fileContents;
+
+echo '<br><br>';
+
+
+//echo $fc;
+
+//echo '<br><br>';
+
+
+$explodedFile = (explode('", "', $fc));
+
+$arrLen = count($explodedFile) - 1;
+
+
+
+for ($i = 0; $i <= $arrLen; $i++) {
+  echo "Current Key is: $i <br>";
+  echo $explodedFile[$i];
+  echo "<br><br>";
+}
+
+
+//echo $explodedFile[0];
+//echo 'file contents: ' . $fileContents . " |||output done";
+//echo md5($fileContents);
+
+
+//$loadedJsonFile = json_encode($fileContents);
+
+//echo $loadedJsonFile;
+//echo gettype($loadedJsonFile);
+//echo '<br><br>';
+//echo gettype($fileContents);
+
+
+//echo ;
+
+/*
+echo $fileContents;
+echo '<br><br>';
+
+
+//$fileLength = count($fileContents);
+//echo 'File length: ' . $fileLength;
+//$fileSplitLen = $fileLength / 7;
+
+echo '<br><br>';
+
+$explodedFile = (explode('", "', $fileContents));
+
+echo $explodedFile;
+echo $explodedFile[0];
+
+
+for ($i = 0; $i <= 10; $i++) {
+  echo "The number is: $i <br>";
+  echo $fileContents['"' . $i . '"'];
+  echo "<br><br>";
+}
+*/
+
+
+echo '<html>';
+echo '<div class="ioHeaderMenu">';
+  echo '<button class="button ioHeaderMenuObject" onclick="window.location.href=""https://shardhive.com"">shardHive.com</button>';
+
+  echo '<button class="button ioHeaderMenuObject" onclick="window.location.href="https://io.shardhive.com/"">IO Landing</button>';
   
-  $sql1 = "SELECT username, pass, confirmPass FROM jTest";
-  $result = $conn->query($sql1);
-  if ($result->num_rows > 0) {
-      while($row = $result -> fetch_assoc()) {
-          $pass0=$row["pass"];
-          $pass1=$row["confirmPass"];
-          if($pass0 == $pass1){
-            echo "<h2>Success Creating Account for : " . $row["username"] . "</h2>"
-          } else{
-              echo "<h2>Try Again. Passwords do not match</h2>";
-          }
-      }
-  } else {
-      echo "0 results";
-  }
+  echo '<button class="button ioHeaderMenuObject" onclick="window.location.href=""https://io.shardhive.com/login.php"">IO Login</button>';
 
-  */
-
-  $conn->close();
-  ?>
+echo '</div>';
+echo '</html>';
 
 
-
-
-</html>
+?>
