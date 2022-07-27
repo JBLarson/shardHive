6d679e1849ccc928c4317ea5626935465500d3d2
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-26 23:20:57 -0400

    Update t0.py

--- src/id/t0.py
+++ src/id/t0.py
@@ -1,17 +1,69 @@
+#!/usr/bin/python3
+
+from shardHiveEVM import *
+from secrets import currentShContract
+import json
 from time import sleep
 import sys
 from mfrc522 import SimpleMFRC522
-reader = SimpleMFRC522()
+
+
+rfidReader = SimpleMFRC522()
+
+
+
+
+
+def writeJson(jsonOutAddr, jsonData):
+	try:
+		with open(jsonOutAddr, 'w') as fp1: json.dump(jsonData, fp1)
+		functionOutput = ("\nSuccess Creating JSON at: " + str(jsonOutAddr) + "\n")
+
+	except Exception as e:
+		functionOutput = "\nFailed to create JSON. Error msg:\n" + str(e)
+
+	return functionOutput
+
+
+
+
+
+print("\n-----------------------------------------------")
+print("Launched shardHive v0.0.2 RFID Terminal")
+print("-----------------------------------------------\n")
+
+id, content = rfidReader.read()
+
+print('RFID ID: ' + str(id))
+
+print('RFID Content: ' + str(content))
+
+content0 = content.strip()
+
+
+
+
+ethBalance = fetchETHBalance(content0)
+print('\nRopsten Test-Net Ether: ' + str(ro4(ethBalance['bal'])))
+
+
+
+altBalance = fetchAltBalance(content0, currentShContract)
+print('shardHive v0.0.2 Test Tokens: ' + str(altBalance))
+
+
+
+
+addressDict = {"rfidId": str(id), "address": content0}
+
+
 
 
 try:
-    while True:
-        print("Hold a tag near the reader")
-        id, text = reader.read()
-        print("ID: %s\nText: %s" % (id,text))
-        sleep(5)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    raise
+    print(writeJson('jTest.json', addressDict))
 
+except Exception as e:
+    print('\nFailed to save address')
+    print(e)
+    pass
 
