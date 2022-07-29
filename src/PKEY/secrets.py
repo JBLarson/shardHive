f5050967f1d6f3c8c5adfd7ba4199509ee6baef0
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-29 03:12:38 -0400

    Delete secrets.py

--- src/PKEY/secrets.py
+++
@@ -1,4 +0,0 @@
-infuraUrl = 'INFURA-URL'
-pKeys = {'userName': 'private_key_address'}
-pubKeys = {'userName': 'public_key_address'}
-servers = {'serverName': {'host': 'serverHost', 'user': 'serverUser', 'ipAddress': 'serverIp', 'pass': 'serverPassword', 'address': 'public_key_address'}}
