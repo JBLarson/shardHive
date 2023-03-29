b22c808779c223d5dcb01c04c685371fb651007e
JB Larson <68449234+JBLarson@users.noreply.github.com>
2023-03-28 21:46:21 -0400

    Create secrets.py

---
+++ src/rfid/secrets.py
@@ -0,0 +1,20 @@
+
+pubKeys = {"shardHive0": "0x983C36a518c56b0FCE99AA8a4aed40913DC469CE",
+            "shardHive1": "0x2C2E5b824EA2BcE625943aF15e1bBD86630B37EF",
+            "shardHive2": "0xc838c62098f8c4597908be707130f63ecec2ffb7"}
+
+
+pKeys = {"shardHive0": "fc249a644e1db4b248ee95986735b46e46377ddd1918161e8049b68df2192138",
+         "shardHive1": "573b5e055d936fdffe337aa5104aaf73c9d1f1659cad2f07fd924a0ec85fc98d",
+         "shardHive2": "740c6ae73482c8ff7488724f36d787875d7c3d8ecc4f9e875bd836a8bd185c54"}
+
+
+
+infuraUrl = "INFURA-API-URL"
+
+
+
+
+altCoinAddress = "0x3A04F7238010Abf1B1FdF30995ceC75f9fEfa220"
+currentShContract = altCoinAddress
+
